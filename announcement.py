1607263171299441fb1a22541d1de982536346eb
Kevin <khchen@github>
2023-08-26 10:14:36 -0400

    first commit - working Grove Bot with announcement command

---
+++ announcement.py
@@ -0,0 +1,103 @@
+import discord
+from discord import app_commands
+import asyncio
+import datetime
+from functools import reduce
+
+import config
+import members
+
+GUILD_CREATED_ON = datetime.date(2021, 12, 19)
+ANNOUNCEMENT_CHANNEL_ID = config.ANNOUNCEMENT_CHANNEL_ID
+
+async def send_announcement(bot, ctx, emoji_id: str, custom_message_id: str):
+    print(emoji_id)
+
+    today = datetime.date.today()
+    sunday = today - datetime.timedelta(days=(today.weekday()+1)%7)
+    guild_week = (sunday - GUILD_CREATED_ON).days // 7
+    leaderboard_week = sunday.strftime('%U')
+    
+    # Confirmation
+    emoji = next(e for e in bot.emojis if str(e) == emoji_id)
+    print(emoji)
+    print(emoji.id)
+    if (emoji is None):
+        await ctx.send('Error - invalid emoji, please use an emoji from this server. Announcement has been cancelled.')
+        return
+
+    custom_message = None
+    if (custom_message_id is not None):
+        custom_message = await ctx.fetch_message(custom_message_id)
+
+    confirmation_message_body = f'Are you sure you want to send the announcement?\n\nWeek {guild_week}\n{sunday}\n{sunday.year} Leaderboard Week {leaderboard_week}\n\n'
+    if custom_message:
+        confirmation_message_body += f'Custom message:\n```{custom_message.content}```\n\n'
+    confirmation_message_body += f'React with {emoji_id} to proceed.'
+
+    confirmation_message = await ctx.send(confirmation_message_body)
+    await confirmation_message.add_reaction(emoji)
+
+    def check(reaction, user):
+        print(reaction)
+        return user == ctx.author and str(reaction.emoji) == emoji_id
+
+    try:
+        reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+    except asyncio.TimeoutError:
+        await ctx.send('Error - confirmation expired. Announcement has been cancelled.')
+        return
+    else:
+        await ctx.send(f'Sending the announcement in <#{ANNOUNCEMENT_CHANNEL_ID}>')
+
+    # Defer because sending the announcement takes longer than 3 seconds
+    #await ctx.defer()
+
+    # Validate the spreadsheet has a column for this week's announcement
+    if not members.is_valid(guild_week, sunday.strftime('%Y-%m-%d')):
+        await ctx.send('Error - unable to find the member tracking data for this week\'s announcement. Announcement has been cancelled.')
+        return
+    
+
+    # Create and format announcement message
+    announcement_body = f'<@&922334019146899566>\n\nThanks everyone for another great week of Grove! Here\'s our week {guild_week} recap:\n<#LEADERBOARD_THREAD_ID_HERE>\n\n'
+
+    new_members = members.get_new_members()
+    match len(new_members):
+        case 0:
+            None
+        case 1:
+            announcement_body += f'Welcome to our new member this week:\n{new_members[0]}\n\n'
+        case _:
+            announcement_body += 'Welcome to our new members this week:\n'
+            announcement_body = reduce(lambda body, member: body + f'{member}\n', new_members, announcement_body)
+            announcement_body += '\n'
+    
+    if (custom_msg_id is not None):
+        msg = await ctx.fetch_message(custom_msg_id)
+        announcement_body += f'{msg.content}\n\n'
+
+    announcement_body += f'Happy Mapling, Go Grove! {emoji_id}'
+
+    # Send announcement
+    send_channel = bot.get_channel(ANNOUNCEMENT_CHANNEL_ID)
+    announcement_message = await send_channel.send(announcement_body)
+
+    # Create leaderboard thread and add link to main announcement
+    leaderboard_thread_title = f'{sunday.year} Culvert & Flag Race Leaderboard - Week {leaderboard_week}'
+    leaderboard_thread = await announcement_message.create_thread(name=leaderboard_thread_title)
+    announcement_body = announcement_body.replace('LEADERBOARD_THREAD_ID_HERE', f'{leaderboard_thread.id}')
+    print(announcement_body)
+    await announcement_message.edit(content=announcement_body)
+
+    # Send leaderboard ranking messages
+    await announce_leaderboard(leaderboard_thread, leaderboard_thread_title)
+
+    await ctx.reply("Done!")
+
+async def announce_leaderboard(leaderboard_thread, leaderboard_thread_title):
+    await leaderboard_thread.send(f'**{leaderboard_thread_title}**')
+    leaderboard = members.get_leaderboard()
+    for line in leaderboard:
+        await leaderboard_thread.send(line)
+    await leaderboard_thread.send('*If you notice an error or have any questions or feedback, please let a <@&918725961061658656> know. Thank you!*')
---
+++ members.py
@@ -0,0 +1,114 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import config
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+SHEET_MEMBER_TRACKING = config.SHEET_MEMBER_TRACKING # The ID of the member tracking sheet
+RANGE_MEMBERS = 'Member List!D3:E'
+RANGE_LEADERBOARD = 'Weekly Participation!A2:F'
+RANGE_WEEK_HEADER = 'Weekly Participation!N1'
+
+
+def get_sheet():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        return sheet
+    except HttpError as err:
+        print(err)
+        return None
+
+def is_valid(week, datestr):
+    sheet = get_sheet()
+    result = sheet.values().get(spreadsheetId=SHEET_MEMBER_TRACKING,
+                                    range=RANGE_WEEK_HEADER).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+        return
+
+    header = values[0][0]
+    return f'Week {week}' in header and datestr in header
+
+def get_new_members():
+    sheet = get_sheet()
+    result = sheet.values().get(spreadsheetId=SHEET_MEMBER_TRACKING,
+                                    range=RANGE_MEMBERS).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+        return []
+
+    new_members = list(filter(lambda l:len(l)==1, values))
+    return [item for sublist in new_members for item in sublist] # flatten
+
+def get_leaderboard():
+    sheet = get_sheet()
+    result = sheet.values().get(spreadsheetId=SHEET_MEMBER_TRACKING,
+                                    range=RANGE_LEADERBOARD).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+        return
+
+    filtered = list(filter(lambda l:len(l)==6 and int(l[4]) > 0, values))
+    ordered_list = sorted(filtered, key=lambda l:float(l[0]))
+    sorted_list = sorted(ordered_list, key=lambda l:float(l[4]), reverse=True)
+
+    output = []
+    current_score = None
+    line = ''
+    for member in sorted_list:
+        index = member[0]
+        score = member[4]
+        discord_id = member[5]
+        # print(f'{index}, {score}, {discord_id}')
+
+        if score != current_score:
+            if current_score is not None:
+                output.append(line)
+            line = f'{score} '
+        line += f'{discord_id} '
+        current_score = score
+
+    if current_score is not None:
+        output.append(line)
+
+    # print(output)
+    return output
