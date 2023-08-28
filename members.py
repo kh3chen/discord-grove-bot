0f728aa5b5ef0a240e030681e5577939e95fb8bf
Kevin <khchen@github>
2023-08-27 22:22:40 -0400

    refactor sheets service into a separate class

--- members.py
+++ members.py
@@ -1,17 +1,5 @@
-from __future__ import print_function
-
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
 import config
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+import sheets
 
 SHEET_MEMBER_TRACKING = config.SHEET_MEMBER_TRACKING  # The ID of the member tracking sheet
 RANGE_MEMBERS = 'Member List!D3:E'
@@ -19,43 +7,10 @@ RANGE_LEADERBOARD = 'Weekly Participation!A2:F'
 RANGE_WEEK_HEADER = 'Weekly Participation!N1'
 
 
-def get_sheet():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        service = build('sheets', 'v4', credentials=creds)
-
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-        return sheet
-    except HttpError as err:
-        print(err)
-        return None
-
-
 def is_valid(week, datestr):
-    sheet = get_sheet()
-    result = sheet.values().get(spreadsheetId=SHEET_MEMBER_TRACKING,
-                                range=RANGE_WEEK_HEADER).execute()
+    service = sheets.get_service()
+    result = service.spreadsheets().values().get(spreadsheetId=SHEET_MEMBER_TRACKING,
+                                                 range=RANGE_WEEK_HEADER).execute()
     values = result.get('values', [])
 
     if not values:
@@ -67,8 +22,8 @@ def is_valid(week, datestr):
 
 
 def get_new_members():
-    sheet = get_sheet()
-    result = sheet.values().get(spreadsheetId=SHEET_MEMBER_TRACKING,
+    service = sheets.get_service()
+    result = service.spreadsheets().values().get(spreadsheetId=SHEET_MEMBER_TRACKING,
                                 range=RANGE_MEMBERS).execute()
     values = result.get('values', [])
 
@@ -81,8 +36,8 @@ def get_new_members():
 
 
 def get_leaderboard():
-    sheet = get_sheet()
-    result = sheet.values().get(spreadsheetId=SHEET_MEMBER_TRACKING,
+    service = sheets.get_service()
+    result = service.spreadsheets().values().get(spreadsheetId=SHEET_MEMBER_TRACKING,
                                 range=RANGE_LEADERBOARD).execute()
     values = result.get('values', [])
 
