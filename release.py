9fdac8986536714a84381edee2f934966973226e
Kevin <khchen@github>
2023-09-07 12:42:37 -0400

    release: bump version to 1.0.6-alpha1

--- release.py
+++ release.py
@@ -1,6 +1,6 @@
 major = 1
 minor = 0
-patch = 5
-pre_release = 'alpha04'
+patch = 6
+pre_release = 'alpha1'
 
 version_name = f'Version {major}.{minor}.{patch}{"-" + pre_release if pre_release else ""}'
