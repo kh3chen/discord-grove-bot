86e158c579ed62b5dda42175237d400e6a2cfc4b
Kevin <khchen@github>
2023-09-24 20:23:55 -0400

    fix: rename announcementz command to announcement

--- announcement/cog.py
+++ announcement/cog.py
@@ -9,7 +9,7 @@ class AnnouncementCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name='announcementz', description='Sends the weekly Grove announcement')
+    @app_commands.command(name='announcement', description='Sends the weekly Grove announcement')
     @app_commands.checks.has_role(config.GROVE_ROLE_ID_JUNIOR)
     @app_commands.describe(emoji='The seasonal Grove tree emoji')
     @app_commands.describe(custom_msg_id='The message ID you want to copy for the custom announcement')
