a5304a2251779f875bee6f93cfba224344dd4c37
Kevin <khchen@github>
2023-09-30 20:16:42 -0400

    release: bump version to 1.3.0

--- utils/version.py
+++ utils/version.py
@@ -1,6 +1,5 @@
 major = 1
-minor = 2
+minor = 3
 patch = 0
-pre_release = 'alpha01'
 
-version_name = f'Version {major}.{minor}.{patch}{"-" + pre_release if pre_release else ""}'
+version_name = f'Version {major}.{minor}.{patch}'
