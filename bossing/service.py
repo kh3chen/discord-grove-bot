20ac42d61a8da4f07d2aed489f046cbd760e743e
Kevin <khchen@github>
2026-03-31 11:34:29 -0400

    log: add log file for bossing service to debug

--- bossing/service.py
+++ bossing/service.py
@@ -7,6 +7,11 @@ from bossing.sheets import Party as SheetsParty
 from utils.constants import SEVEN_DAYS_IN_SECONDS
 
 
+def log(message):
+    with open("bossing-service.log", "a") as f:
+        f.write(f'{datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")} {message}\n')
+
+
 class BossTimeService:
     CHECK_IN_OFFSET = -86400  # 24 hours before
     REMINDER_ONE_OFFSET = -21600  # 6 hours before
@@ -59,36 +64,44 @@ class BossTimeService:
         self.updater_task = asyncio.create_task(self.service_loop(sheets_party))
 
     async def service_loop(self, sheets_parties: list[SheetsParty]):
-        # Create events list
-        self.events = []
-        now = int(datetime.timestamp(datetime.now()))
-        print('boss service loop start')
-        for sheets_party in sheets_parties:
-            await self.add_party_events(self.events, now, sheets_party)
-
-        while True:
-            # Sleep until next event
-            print(f'Next 5 bossing events: {self.events[0:5]}')
+        try:
+            # Create events list
+            self.events = []
             now = int(datetime.timestamp(datetime.now()))
-            sleep_duration = self.events[0].timestamp - now
-            if sleep_duration > 0:
-                print(f'Bossing service sleeping for {sleep_duration} seconds.')
-                await asyncio.sleep(sleep_duration)
+            print('boss service loop start')
+            for sheets_party in sheets_parties:
+                await self.add_party_events(self.events, now, sheets_party)
 
-            # Fire event
-            event = self.events.pop(0)
-            if event.update_type == BossTimeService.Event.Type.check_in:
-                await self.on_check_in(event.sheets_party)
-            elif event.update_type == BossTimeService.Event.Type.check_in_reminder:
-                await self.on_check_in_reminder(event.sheets_party)
-            elif event.update_type == BossTimeService.Event.Type.requested_reminder:
-                await self.on_requested_reminder(event.sheets_party)
-            elif event.update_type == BossTimeService.Event.Type.run_start:
-                await self.on_run_start(event.sheets_party)
-            elif event.update_type == BossTimeService.Event.Type.update:
-                await self.on_update(event.sheets_party)
-                # Add party events for next scheduled run
-                await self.add_party_events(self.events, int(datetime.timestamp(datetime.now())), event.sheets_party)
+            while True:
+                # Sleep until next event
+                print(f'Next 5 bossing events: {self.events[0:5]}')
+                now = int(datetime.timestamp(datetime.now()))
+                sleep_duration = self.events[0].timestamp - now
+                if sleep_duration > 0:
+                    print(f'Bossing service sleeping for {sleep_duration} seconds.')
+                    log(f'Bossing service sleeping for {sleep_duration} seconds.')
+                    await asyncio.sleep(sleep_duration)
+
+                # Fire event
+                event = self.events.pop(0)
+                log(event)
+                if event.update_type == BossTimeService.Event.Type.check_in:
+                    await self.on_check_in(event.sheets_party)
+                elif event.update_type == BossTimeService.Event.Type.check_in_reminder:
+                    await self.on_check_in_reminder(event.sheets_party)
+                elif event.update_type == BossTimeService.Event.Type.requested_reminder:
+                    await self.on_requested_reminder(event.sheets_party)
+                elif event.update_type == BossTimeService.Event.Type.run_start:
+                    await self.on_run_start(event.sheets_party)
+                elif event.update_type == BossTimeService.Event.Type.update:
+                    await self.on_update(event.sheets_party)
+                    # Add party events for next scheduled run
+                    await self.add_party_events(self.events, int(datetime.timestamp(datetime.now())),
+                                                event.sheets_party)
+        except Exception as e:
+            print(e)
+            log(e)
+            log(self.events)
 
     async def add_party_events(self, events, now, sheets_party):
         next_scheduled_time = sheets_party.next_scheduled_time()
@@ -96,60 +109,58 @@ class BossTimeService:
             # Check-in
             check_in_time = next_scheduled_time + BossTimeService.CHECK_IN_OFFSET
             if check_in_time - now > 0:
-                self.__insert_event(events,
-                                    BossTimeService.Event(check_in_time,
-                                                          BossTimeService.Event.Type.check_in,
-                                                          sheets_party))
+                check_in_event = BossTimeService.Event(check_in_time,
+                                                       BossTimeService.Event.Type.check_in,
+                                                       sheets_party)
+                self.__insert_event(events, check_in_event)
+                log(f'Add event: {check_in_event}')
 
             # Reminder 1
             reminder_one_time = next_scheduled_time + BossTimeService.REMINDER_ONE_OFFSET
             if reminder_one_time - now > 0:
-                self.__insert_event(events,
-                                    BossTimeService.Event(reminder_one_time,
-                                                          BossTimeService.Event.Type.check_in_reminder,
-                                                          sheets_party))
+                reminder_one_event = BossTimeService.Event(reminder_one_time,
+                                                           BossTimeService.Event.Type.check_in_reminder,
+                                                           sheets_party)
+                self.__insert_event(events, reminder_one_event)
+                log(f'Add event: {reminder_one_event}')
 
             # Reminder 2
             reminder_two_time = next_scheduled_time + BossTimeService.REMINDER_TWO_OFFSET
             if reminder_two_time - now > 0:
-                self.__insert_event(events,
-                                    BossTimeService.Event(reminder_two_time,
-                                                          BossTimeService.Event.Type.check_in_reminder,
-                                                          sheets_party))
+                reminder_two_event = BossTimeService.Event(reminder_two_time,
+                                                           BossTimeService.Event.Type.check_in_reminder,
+                                                           sheets_party)
+                self.__insert_event(events, reminder_two_event)
+                log(f'Add event: {reminder_two_event}')
 
             # Reminder 3
             reminder_three_time = next_scheduled_time + BossTimeService.REMINDER_THREE_OFFSET
             if reminder_three_time - now > 0:
-                self.__insert_event(events,
-                                    BossTimeService.Event(reminder_three_time,
-                                                          BossTimeService.Event.Type.check_in_reminder,
-                                                          sheets_party))
+                reminder_three_event = BossTimeService.Event(reminder_three_time,
+                                                             BossTimeService.Event.Type.check_in_reminder,
+                                                             sheets_party)
+                self.__insert_event(events, reminder_three_event)
+                log(f'Add event: {reminder_three_event}')
 
             # Requested reminder
             requested_reminder_time = next_scheduled_time + BossTimeService.REQUESTED_REMINDER_OFFSET
             if requested_reminder_time - now > 0:
-                self.__insert_event(events,
-                                    BossTimeService.Event(requested_reminder_time,
-                                                          BossTimeService.Event.Type.requested_reminder,
-                                                          sheets_party))
-
-            # # Boss run
-            # boss_run_time = next_scheduled_time
-            # if boss_run_time - now > 0:
-            #     self.__insert_event(events,
-            #                         BossTimeService.Event(boss_run_time,
-            #                                               BossTimeService.Event.Type.run_start,
-            #                                               sheets_party))
+                requested_reminder_event = BossTimeService.Event(requested_reminder_time,
+                                                                 BossTimeService.Event.Type.requested_reminder,
+                                                                 sheets_party)
+                self.__insert_event(events, requested_reminder_event)
+                log(f'Add event: {requested_reminder_event}')
 
             # Update
             update_time = next_scheduled_time + BossTimeService.UPDATE_OFFSET
             if update_time - now > SEVEN_DAYS_IN_SECONDS:
                 # Update time is more than 7 days away, so now is within an hour of the last scheduled run before its corresponding update
                 update_time -= SEVEN_DAYS_IN_SECONDS
-            self.__insert_event(events,
-                                BossTimeService.Event(update_time,
-                                                      BossTimeService.Event.Type.update,
-                                                      sheets_party))
+            update_event = BossTimeService.Event(update_time,
+                                                 BossTimeService.Event.Type.update,
+                                                 sheets_party)
+            self.__insert_event(events, update_event)
+            log(f'Add event: {update_event}')
 
     def get_event_logs(self, event_count: int):
         return [event.log_string() for event in self.events[0:event_count]]
